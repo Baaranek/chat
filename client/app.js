922560b6dd5636cf868066a0561ceec1f6c55d18
Krystian Otto <otto.krystiann@gmail.com>
2020-10-20 12:23:47 +0200

    add app.js

---
+++ client/app.js
@@ -0,0 +1,48 @@
+const loginForm = document.getElementById('welcome-form');
+const messagesSection = document.getElementById('messages-section'); 
+const messagesList = document.getElementById('messages-list');
+const addMessageForm = document.getElementById('add-messages-form');
+const userNameInput = document.getElementById('username');
+const messageContentInput = document.getElementById('message-content');
+
+let userName;
+
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  login();
+});
+
+function login(){
+  if(userNameInput.value){
+    userName = userNameInput.value;
+    loginForm.classList.remove('show');
+    messagesList.classList.add('show');
+    messagesSection.classList.add('show');
+  }else alert('PANIE ERROR WPISAJ COŚ TU');
+}
+
+addMessageForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  sendMessage();
+});
+
+function sendMessage(){
+  if(!messageContentInput.value){
+    alert('PANIE ERROR WPISAJ COŚ TU');
+  }else
+  addMessage(userName, messageContentInput.value);
+  messageContentInput.value = '';
+};
+
+function addMessage(user, content){
+  const message = document.createElement('li');
+  message.className = 'message message--received';
+  userName ? message.classList.add('message--self') : null;
+
+  message.innerHTML = `
+  <h3 class="message__author">${user === userName ? 'You' : user}</h3>
+  <div class="message__content">${content}</div>
+  `;
+
+  messagesList.appendChild(message);
+}
